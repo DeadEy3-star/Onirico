16e3feed10fed1b8709eb97eca7d32d3752f0c39
DeadEy3-star <67383582+DeadEy3-star@users.noreply.github.com>
2022-07-24 02:20:42 -0500

    Movimiento

--- Assets/Scripts/MainCharacterScript.cs
+++ Assets/Scripts/MainCharacterScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MainCharacterScript : MonoBehaviour
 {
+    public float movementSpeed;
+    public Rigidbody2D RB;
+    private Vector2 MoveDirecction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,32 @@ public class MainCharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessInputs();
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        MoveDirecction = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        RB.velocity = new Vector2(MoveDirecction.x * movementSpeed, MoveDirecction.y * movementSpeed);
+    }
+
+    private void OnAnimatorMove()
+    {
         
     }
+
+
 }
